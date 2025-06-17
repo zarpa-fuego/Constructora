bf6163ac970fa23fdf433df8fbcbeed344270e4a
Bonnier Nilss Rodrigo <61536276@upeu.edu.pe>
2025-06-17 18:47:42 -0500

    Areglos

--- resources/views/base/sidebar.blade.php
+++ resources/views/base/sidebar.blade.php
@@ -6,92 +6,94 @@
 
         <ul class="sidebar-nav">
             <li class="sidebar-header">
-                Pages
+                Navegación
             </li>
 
             <li class="sidebar-item active">
                 <a class="sidebar-link" href="{{route('welcome')}}">
-                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                    <i class="align-middle" data-feather="sliders"></i> <span
+                        class="align-middle">Gestión de Lotes</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{route('listar_venta')}}">
-                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Ventas</span>
+                    <i class="align-middle" data-feather="user"></i> <span
+                        class="align-middle">Gestión de Clientes</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link" href="pages-sign-in.html">
-                    <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                    <i class="align-middle" data-feather="log-in"></i> <span
+                        class="align-middle">Gestión de Reservas</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link" href="pages-sign-up.html">
-                    <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+                    <i class="align-middle" data-feather="user-plus"></i> <span
+                        class="align-middle">Registro de Ventas</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link" href="pages-blank.html">
-                    <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+                    <i class="align-middle" data-feather="book"></i> <span class="align-middle">Gestión de Pagos</span>
                 </a>
-            </li>
-
-            <li class="sidebar-header">
-                Tools & Components
             </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link" href="ui-buttons.html">
-                    <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+                    <i class="align-middle" data-feather="square"></i> <span
+                        class="align-middle">Gestión de Usuarios</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link" href="ui-forms.html">
-                    <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+                    <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Informes</span>
                 </a>
             </li>
+            <!--
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="ui-cards.html">
+                                <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+                            </a>
+                        </li>
 
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="ui-cards.html">
-                    <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-                </a>
-            </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="ui-typography.html">
+                                <i class="align-middle" data-feather="align-left"></i> <span
+                                    class="align-middle">Typography</span>
+                            </a>
+                        </li>
 
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="ui-typography.html">
-                    <i class="align-middle" data-feather="align-left"></i> <span
-                        class="align-middle">Typography</span>
-                </a>
-            </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="icons-feather.html">
+                                <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+                            </a>
+                        </li>
 
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="icons-feather.html">
-                    <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-                </a>
-            </li>
+                        <li class="sidebar-header">
+                            Plugins & Addons
+                        </li>
 
-            <li class="sidebar-header">
-                Plugins & Addons
-            </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="charts-chartjs.html">
+                                <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+                            </a>
+                        </li>
 
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="charts-chartjs.html">
-                    <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
-                </a>
-            </li>
-
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="maps-google.html">
-                    <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
-                </a>
-            </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="maps-google.html">
+                                <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+                            </a>
+                        </li>
+                        -->
         </ul>
 
-        <div class="sidebar-cta">
+        <!--<div class="sidebar-cta">
             <div class="sidebar-cta-content">
                 <strong class="d-inline-block mb-2">Upgrade to Pro</strong>
                 <div class="mb-3 text-sm">
@@ -101,6 +103,7 @@
                     <a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
                 </div>
             </div>
-        </div>
+-->
     </div>
+
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,115 +6,13 @@
     <main class="content">
         <div class="container-fluid p-0">
 
-            <h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
-
-            <div class="row">
-                <div class="col-xl-6 col-xxl-5 d-flex">
-                    <div class="w-100">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Sales</h5>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="truck"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h1 class="mt-1 mb-3">2.382</h1>
-                                        <div class="mb-0">
-                                            <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Visitors</h5>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="users"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h1 class="mt-1 mb-3">14.212</h1>
-                                        <div class="mb-0">
-                                            <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.25% </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Earnings</h5>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="dollar-sign"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h1 class="mt-1 mb-3">$21.300</h1>
-                                        <div class="mb-0">
-                                            <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col mt-0">
-                                                <h5 class="card-title">Orders</h5>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <div class="stat text-primary">
-                                                    <i class="align-middle" data-feather="shopping-cart"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <h1 class="mt-1 mb-3">64</h1>
-                                        <div class="mb-0">
-                                            <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.25% </span>
-                                            <span class="text-muted">Since last week</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-6 col-xxl-7">
-                    <div class="card flex-fill w-100">
-                        <div class="card-header">
-
-                            <h5 class="card-title mb-0">Recent Movement</h5>
-                        </div>
-                        <div class="card-body py-3">
-                            <div class="chart chart-sm">
-                                <canvas id="chartjs-dashboard-line"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <h1 class="h3 mb-3"><strong>Gestionar</strong> Lotes</h1>
+            <div>
+                <button>Exportar</button>
+                <button>Nuevo Lote</button>
             </div>
-
+            </div>
             <div class="row">
                 <div class="col-12 col-md-6 col-xxl-3 d-flex order-2 order-xxl-3">
                     <div class="card flex-fill w-100">
@@ -177,7 +75,6 @@
                     </div>
                 </div>
             </div>
-
             <div class="row">
                 <div class="col-12 col-lg-8 col-xxl-9 d-flex">
                     <div class="card flex-fill">
